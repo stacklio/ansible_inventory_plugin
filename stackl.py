accd6b65c897e0875a67c7f867934f2f37fa33aa
Frederic <frederic.vanreet@icloud.com>
2020-03-09 11:54:31 +0100

    Kubernetes inventory

--- stackl.py
+++ stackl.py
@@ -6,6 +6,7 @@ name: stackl
 plugin_type: inventory
 author:
   - Stef Graces <@stgrace>
+  - Frederic Van Reet <@GBrawl>
 short_description: Stackl inventory
 description:
   - Fetch a Stack instance from Stackl.
@@ -30,7 +31,7 @@ stack_instance: "test_vm"
 '''
 
 import json
-import stackl_sdk
+import stackl_client
 
 from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.plugins.inventory import BaseInventoryPlugin
@@ -53,21 +54,29 @@ class InventoryModule(BaseInventoryPlugin):
     self._read_config_data(path)
     try:
       self.plugin = self.get_option('plugin')
-      configuration = stackl_sdk.Configuration()
+      configuration = stackl_client.Configuration()
       configuration.host = self.get_option("host")
-      api_client = stackl_sdk.ApiClient(configuration=configuration)
-      api_instance = stackl_sdk.StackInstancesApi(api_client=api_client)
+      api_client = stackl_client.ApiClient(configuration=configuration)
+      api_instance = stackl_client.StackInstancesApi(api_client=api_client)
 
       stack_instance_name = self.get_option("stack_instance")
-
       stack_instance = api_instance.get_stack_instance(stack_instance_name)
-      self.inventory.add_group(stack_instance_name)
-      for service in stack_instance.services.keys():
-        self.inventory.add_host(host=service, group=stack_instance_name)
+
+      for service, si_service in stack_instance.services.items():
+        self.inventory.add_group(service)
         self.inventory.set_variable(service, "infrastructure_target", 
-                                    stack_instance.services[service].infrastructure_target)
+                                    si_service.infrastructure_target)
         self.inventory.set_variable(service, "provisioning_parameters",
-                                    stack_instance.services[service].provisioning_parameters)
+                                    si_service.provisioning_parameters)
+        if si_service.hosts is not None:
+          for host in si_service.hosts:
+            self.inventory.add_host(host=host, group=service)
+            self.inventory.set_variable(service, "ansible_user", si_service.connection_credentials.username)
+            self.inventory.set_variable(service, "ansible_password", si_service.connection_credentials.password)
+            self.inventory.set_variable(service, "ansible_become_password", si_service.connection_credentials.password)
+            self.inventory.set_variable(service, "ansible_ssh_common_args", "-o StrictHostKeyChecking=no")
+        else:
+          self.inventory.add_host(host="kubernetes-" + service, group=service)                    
 
     except Exception as e:
       raise AnsibleParserError(
